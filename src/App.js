5ef0c63f5a60ed54a0c5ae17fb62f59e4e845f55
srinivas.reddy.94807@gmail.com <srinivas.reddy.94807@gmail.com>
2021-12-10 12:46:27 +0530

    adding files to new repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from "react";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+let name = "Srinivas";
 function App() {
+  const [mode, setMode] = useState(false);
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        aboutText="About"
+        toggleMode={toggleMode}
+      ></Navbar>
+      <div className="container">
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm heading="Enter the text toa analyse"></TextForm>
+          </Route>
+        </Switch>
+
+        <About></About>
+      </div>
+    </Router>
   );
 }
 
